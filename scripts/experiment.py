f16195e1b9ab0a3ffc6c59123d5eefed7712b5ca
Jannik Kissinger <jannik.kissinger@sva.de>
2023-07-04 16:40:39 +0200

    Update experiment.py

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -11,11 +11,8 @@
 #     language: python
 #     name: python3
 # ---
-import os
-from pathlib import Path
 
-# %% [raw]
-#
+# %%
 
 # %% [markdown]
 # # Income Tax Analysis NY
@@ -24,12 +21,13 @@ from pathlib import Path
 
 # %%
 # %matplotlib inline
-# # %matplotlib notebook
 from matplotlib import pyplot as plt
 import seaborn as sns
 import math
 import pandas as pd
-import numpy as np
+import os
+from pathlib import Path
+import sklearn
 
 PROJECT_ROOT = Path(os.path.abspath('')).parent
 DATA_FOLDER = PROJECT_ROOT / "data"
@@ -42,81 +40,81 @@ COL_CARDINALITY = "Number of Returns"
 COL_COUNTRY = "Country"
 COL_RESIDENCE = "Place of Residence"
 COL_INCOME_RANGE = "NY Adjusted Gross Income Range (Fed.Col)"
-COLS_TO_DROP = ["Sort on Place", "Sort on Income Range", "Notes"]
+COL_SOURCE = "source"
 COLS_CATEGORICAL = ["NYS Residency Status", COL_RESIDENCE, "County", "State", COL_COUNTRY,
                     COL_INCOME_RANGE]
 
-
-
 COLS_MONETARY = [
-    # 'Federal Amount of NY Adjusted Gross Income',
-    #              'New York State Amount of NY Adjusted Gross Income',
-    #              'New York Standard Deductions Claimed',
-    #              'New York Itemized Deductions Claimed', 'Deductions Used',
-    #              'Dependent Exemptions Claimed', 'Dependent Exemptions Used',
-    #              'Taxable Income', 'Tax Before Credits',
-    'Taxable Income',
-    'Tax Liability',  # the payment owed by an entity to a federal, state, or local tax authority
-    'Tax Credits',  # money that taxpayers can subtract directly from the taxes they owe
+    # types of income:
+    # 'Taxable Income',
     'Wage and Salary Income',
-    #              'Interest', 'Dividends',
-    #              'Gain from Capital&Supplemental Income',
-    #              'Loss from Capital&Supplemental Income',
-    #              'Gain from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
-    #              'Loss from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
-    #              'Gain from Business & Farm Income', 'Loss from Business & Farm Income',
-    #              'Pension/Annuity & IRA Income', 'All Other Income',
-    #              'Federal Adjustments', 'Federal Adjusted Gross Income',
-    #              'Additions State & Local Bond Interest',
-    #              'Additions Public Employee Retirement System Contributions',
-    #              'Additions College Choice Tuition Savings Program Distributions',
-    #              'Other NY Additions', 'Subtractions State & Local Income Tax Refunds',
-    #              'Subtractions Government Pension Exclusion',
-    #              'Subtractions Taxable Social Security Income',
-    #              'Subtractions Federal Bond Interest',
-    #              'Subtractions Pension & Annuity Exclusion',
-    #              'Subtractions College Choice Tuition Savings Program Deductions/Earnings',
-    #              'Other NY Subtractions',
-    'Medical & Dental Expenses',
+    'Gain from Capital&Supplemental Income',
+    # 'Loss from Capital&Supplemental Income',
+    'Gain from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
+    # 'Loss from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
+    'Gain from Business & Farm Income',
+    # 'Loss from Business & Farm Income',
+    # 'Pension/Annuity & IRA Income',
+    # 'All Other Income',
+    'Interest',
+    # 'Interest Paid',
+    'Dividends',
+    # 'Charitable Contributions',
+    # 'Medical & Dental Expenses',
     COL_TARGET,
-    #              'Interest Paid', 'Charitable Contributions', 'Other Deductions',
-    #              'Federal Deductions', 'Income Taxes & Subtraction Adjustments',
-    #              'NY Addition Adjustments', 'NY Itemized Deduction Adjustment'
+    # 'Federal Amount of NY Adjusted Gross Income',
+    # 'New York State Amount of NY Adjusted Gross Income',
+    # 'New York Standard Deductions Claimed',
+    # 'New York Itemized Deductions Claimed',
+    # 'Deductions Used',
+    # 'Dependent Exemptions Claimed',
+    # 'Dependent Exemptions Used',
+    # 'Taxable Income',
+    # 'Tax Before Credits',
+    # 'Tax Liability',  # the payment owed by an entity to a federal, state, or local tax authority
+    # 'Tax Credits',  # money that taxpayers can subtract directly from the taxes they owe
+    # 'Federal Adjustments',
+    # 'Federal Adjusted Gross Income',
+    # 'Additions State & Local Bond Interest',
+    # 'Additions Public Employee Retirement System Contributions',
+    # 'Additions College Choice Tuition Savings Program Distributions',
+    # 'Other NY Additions',
+    # 'Subtractions State & Local Income Tax Refunds',
+    # 'Subtractions Government Pension Exclusion',
+    # 'Subtractions Taxable Social Security Income',
+    # 'Subtractions Federal Bond Interest',
+    # 'Subtractions Pension & Annuity Exclusion',
+    # 'Subtractions College Choice Tuition Savings Program Deductions/Earnings',
+    # 'Other NY Subtractions',
+    # 'Charitable Contributions',
+    # 'Other Deductions',
+    # 'Federal Deductions',
+    # 'Income Taxes & Subtraction Adjustments',
+    # 'NY Addition Adjustments',
+    # 'NY Itemized Deduction Adjustment'
 ]
 
-COLS = ['Tax Year', 'NYS Residency Status', 'Place of Residence', 'County',
-        'State', 'Country', 'NY Adjusted Gross Income Range (Fed.Col)',
-        'Number of Returns', 'Federal Amount of NY Adjusted Gross Income',
-        'New York State Amount of NY Adjusted Gross Income',
-        'New York Standard Deductions Claimed',
-        'New York Itemized Deductions Claimed', 'Deductions Used',
-        'Dependent Exemptions Claimed', 'Dependent Exemptions Used',
-        'Taxable Income', 'Tax Before Credits', 'Tax Credits', 'Tax Liability',
-        'Wage and Salary Income', 'Interest', 'Dividends',
-        'Gain from Capital&Supplemental Income',
-        'Loss from Capital&Supplemental Income',
-        'Gain from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
-        'Loss from Rent,Royalties,Prtnrshp,Estates,Trusts Income',
-        'Gain from Business & Farm Income', 'Loss from Business & Farm Income',
-        'Pension/Annuity & IRA Income', 'All Other Income',
-        'Federal Adjustments', 'Federal Adjusted Gross Income',
-        'Additions State & Local Bond Interest',
-        'Additions Public Employee Retirement System Contributions',
-        'Additions College Choice Tuition Savings Program Distributions',
-        'Other NY Additions', 'Subtractions State & Local Income Tax Refunds',
-        'Subtractions Government Pension Exclusion',
-        'Subtractions Taxable Social Security Income',
-        'Subtractions Federal Bond Interest',
-        'Subtractions Pension & Annuity Exclusion',
-        'Subtractions College Choice Tuition Savings Program Deductions/Earnings',
-        'Other NY Subtractions', 'Medical & Dental Expenses', 'Taxes Paid',
-        'Interest Paid', 'Charitable Contributions', 'Other Deductions',
-        'Federal Deductions', 'Income Taxes & Subtraction Adjustments',
-        'NY Addition Adjustments', 'NY Itemized Deduction Adjustment']
+COLS2KEEP = [COL_TAX_YEAR, COL_INCOME_RANGE] + COLS_MONETARY
+
+def get_score(regr, X, y):
+    y_pred = regr.predict(X)
+    score = sklearn.metrics.r2_score(y, y_pred)
+    print("score:", score)
+    return score
+
+
+def plot_predictions(regr, X,y):
+    y_pred = regr.predict(X).flatten()
+    print("number of samples:", len(y_pred))
+    COL_SOURCE = "source"
+    outputs1 = pd.DataFrame({COL_TARGET: y, COL_SOURCE: "actual"})
+    outputs2 = pd.DataFrame({COL_TARGET: y_pred, COL_SOURCE: "predicted"})
+    outputs = pd.concat([outputs1, outputs2])
+    sns.displot(outputs, x=COL_TARGET, hue=COL_SOURCE, element="step")
 
 
 # %% [markdown]
-# Data Preprocessing
+# Data Preprocessing: Cleaning, Feature Selection, Feature Engineering
 
 # %%
 
@@ -130,7 +128,6 @@ def drop_rows_w_missing_values(df):
 
 path = CSV_FILE
 df = pd.read_csv(path)
-df.drop(columns=COLS_TO_DROP, inplace=True)
 df[COL_TAX_YEAR] = df[COL_TAX_YEAR].astype(int)
 df.fillna(0.0, inplace=True)
 
@@ -159,26 +156,34 @@ df = drop_rows_w_missing_values(df)
 # TODO only predict for people who pay taxes -> easier prediction, data less skewed
 df = df[df[COL_TARGET] > 0.0]
 
+df = df[COLS2KEEP]
+
 print("done.")
 print(f"found {len(df)} rows, {len(df.columns)} columns\n")
 
 # %% [markdown]
-# import ydata_profiling
-#
-# profile = ydata_profiling.ProfileReport(df,
-#                                         minimal=True
-#                                         )
-# profile.to_file("data_distribution_report.html")
+# Generate Basic Data Report
 
 # %%
-import seaborn as sns
 
-sns.pairplot(df[COLS_MONETARY].sample(100), diag_kind='kde')
+import ydata_profiling
+
+profile = ydata_profiling.ProfileReport(df,
+                                        minimal=False
+                                        )
+profile.to_file(REPORTS_FOLDER / "data_distribution_report.html")
+
+# sns.pairplot(df[COLS_MONETARY].sample(100), diag_kind='kde')
+# plt.tight_layout()  # prevent clipping
+# plt.savefig(REPORTS_FOLDER/"column_pairplot.png", dpi=200)
+# plt.close()
+
+
+# %% [markdown]
+# Split data into training and test set using the year column
 
 # %%
-COL_SOURCE = "source"
 
-# split based on year
 COLS_INPUT = list(set(COLS_MONETARY) - {COL_TARGET})
 X_train = df[df[COL_TAX_YEAR] == 2015]
 X_test = df[df[COL_TAX_YEAR] == 2016]
@@ -191,21 +196,22 @@ X_test = X_test[COLS_INPUT]
 outputs1 = pd.DataFrame({COL_TARGET: y_train, COL_SOURCE: "training (year 2015)"})
 outputs2 = pd.DataFrame({COL_TARGET: y_test, COL_SOURCE: "test (year 2016)"})
 outputs = pd.concat([outputs1, outputs2])
-
 sns.displot(outputs, x=COL_TARGET, hue=COL_SOURCE, element="step")
+
+
+# %% [markdown]
+# Fit and evaluate a linear model
 
 # %%
 from sklearn.linear_model import LinearRegression
 
 regr = LinearRegression()
 regr.fit(X_train, y_train)
+print("score on test set:", get_score(regr, X_test, y_test))
+plot_predictions(regr, X_test, y_test)
 
-# import joblib
-# joblib.dump(regr, "model.joblib")
-
-# TODO 'Sequential' object has no attribute 'score'
-score = regr.score(X_test, y_test)
-score
+# %% [markdown]
+# Fit and evaluate a simple single layer neural network
 
 # %%
 from sklearn.neural_network import MLPRegressor
@@ -219,14 +225,18 @@ regr.fit(X_train, y_train)
 # import joblib
 # joblib.dump(regr, "model.joblib")
 
-# TODO 'Sequential' object has no attribute 'score'
-score = regr.score(X_test, y_test)
-score
+print("score on test set:", get_score(regr, X_test, y_test))
+plot_predictions(regr, X_test, y_test)
+
+
+# %% [markdown]
+# Fit and evaluate a multi-layer neural network
 
 # %%
 from keras import regularizers
 import tensorflow as tf
 
+tf.random.set_seed(1)
 print("TensorFlow version:", tf.__version__)
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(f"{len(gpus)} GPUs Available:", gpus)
@@ -234,8 +244,7 @@ print(f"{len(gpus)} GPUs Available:", gpus)
 from tensorflow import keras
 from tensorflow.keras import layers
 
-LOSS = 'mean_absolute_error'
-# LOSS = 'mean_squared_error'
+LOSS = 'mean_squared_error' # 'mean_absolute_error'
 ACT_FCT = 'relu'
 DROPOUT = 0.0
 LAYER_SIZE = 20
@@ -246,7 +255,7 @@ batch_size = 128
 lr = 1e-1
 
 test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
-test_dataset = test_dataset.batch(batch_size)
+test_dataset = test_dataset.batch(len(X_test))
 
 
 def build_and_compile_model(norm):
@@ -277,15 +286,24 @@ def build_and_compile_model(norm):
     return model
 
 
-normalizer = tf.keras.layers.Normalization(axis=-1)
+def plot_loss(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel(LOSS)
+    plt.semilogy()
+    plt.legend()
+    plt.grid(True)
 
+
+normalizer = tf.keras.layers.Normalization(axis=-1)
 # fit the state of the preprocessing layer to the data by calling Normalization.adapt:
 normalizer.adapt(X_train)
 
 regr = build_and_compile_model(normalizer)
 print(regr.summary())
 print("training...")
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, mode='min', restore_best_weights=True,
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, mode='min', restore_best_weights=True,
                                             start_from_epoch=10)
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
     monitor='val_loss',
@@ -294,7 +312,7 @@ reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
     verbose=1,
     mode='auto',
     min_delta=0.0,
-    cooldown=5,
+    cooldown=0,
     min_lr=0.0,
 )
 callbacks = [callback, reduce_lr]
@@ -304,59 +322,36 @@ history = regr.fit(
     validation_data=test_dataset,
     verbose=0,
     callbacks=callbacks,
-    epochs=100,  # max
+    epochs=300,  # max
 )
-
 
 # regr.save('dnn_model')
 # reloaded = tf.keras.models.load_model('dnn_model')
 # results = dnn_model.evaluate(test_features, test_labels, verbose=0)
 
 
-def plot_loss(history):
-    # plt.cla()
-    plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['val_loss'], label='val_loss')
-    # plt.ylim([0, 10])
-    plt.xlabel('Epoch')
-    plt.ylabel(LOSS)
-    plt.semilogy()
-    plt.legend()
-    plt.grid(True)
-
-
+print("score on test set:", get_score(regr, X_test, y_test))
 plot_loss(history)
 
 # %%
-y_test_pred = regr.predict(X_test)
 
-import seaborn as sns
+plot_predictions(regr, X_test, y_test)
+# plot_predictions(regr, X_train, y_train)
 
-y_test_pred = y_test_pred.flatten()
-print("y_test_pred.shape", y_test_pred.shape)
-print("y_test.shape", y_test.shape)
-COL_SOURCE = "source"
-outputs1 = pd.DataFrame({COL_TARGET: y_test, COL_SOURCE: "actual"})
-outputs2 = pd.DataFrame({COL_TARGET: y_test_pred, COL_SOURCE: "predicted"})
-outputs = pd.concat([outputs1, outputs2])
-sns.displot(outputs, x=COL_TARGET, hue=COL_SOURCE, element="step")
+
+# %% [markdown]
+# Visualise the prediction errors
 
 # %%
-y_train_pred = regr.predict(X_train)
 
-y_train_pred = y_train_pred.flatten()
-print(f"{y_train.shape=}")
-print(f"{y_train_pred.shape=}")
-COL_SOURCE = "source"
-outputs1 = pd.DataFrame({COL_TARGET: y_train, COL_SOURCE: "actual"})
-outputs2 = pd.DataFrame({COL_TARGET: y_train_pred, COL_SOURCE: "predicted"})
-outputs = pd.concat([outputs1, outputs2])
-
-sns.displot(outputs, x=COL_TARGET, hue=COL_SOURCE, element="step")
-
-# %%
+y_test_pred = regr.predict(X_test).flatten()
 errors = y_test - y_test_pred
 sns.displot(errors, element="step")
+
+
+# %% [markdown]
+# Reduce the dimensionality of the data and visualise it
+
 
 # %%
 import umap
@@ -381,19 +376,8 @@ def add_umap_cols(df, columns, **h_params):
         df[DIM_COLS[d]] = umap_emb
 
 
-# TODO tune
 params = dict(n_neighbors=5, min_dist=0.3)
-# df_sample = df.sample(1000)
-df_sample = df.copy()
-add_umap_cols(df_sample, COLS_MONETARY, **params)
+add_umap_cols(df, COLS_MONETARY, **params)
 
 # %%
-# category_column =COL_COUNTRY
-# category_column =COL_RESIDENCE
-# category_column = "NYS Residency Status"
-# category_column = "County"
-# category_column = "State"
-category_column = COL_INCOME_RANGE
-# COLS_CATEGORICAL = ["NYS Residency Status", COL_RESIDENCE, "County", "State", COL_COUNTRY,
-#                     "NY Adjusted Gross Income Range (Fed.Col)"]
-sns.relplot(data=df_sample, x=DIM_COLS[0], y=DIM_COLS[1], hue=category_column)  # s=size, **kwargs)
+sns.relplot(data=df, x=DIM_COLS[0], y=DIM_COLS[1], hue=COL_INCOME_RANGE)  # s=size, **kwargs)
