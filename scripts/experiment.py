d495f8ce9897180d92ddbfa4570e07d8d6fd3425
Jannik Kissinger <jannik.kissinger@sva.de>
2023-07-05 18:41:00 +0200

    pytorch section

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -12,8 +12,6 @@
 #     name: python3
 # ---
 
-# %%
-
 # %% [markdown]
 # # Income Tax Analysis NY
 #
@@ -27,7 +25,8 @@ import math
 import pandas as pd
 import os
 from pathlib import Path
-import sklearn
+from sklearn.metrics import r2_score
+
 PROJECT_ROOT = Path(os.path.abspath('')).parent
 DATA_FOLDER = PROJECT_ROOT / "data"
 REPORTS_FOLDER = PROJECT_ROOT / "report"
@@ -95,14 +94,15 @@ COLS_MONETARY = [
 
 COLS2KEEP = [COL_TAX_YEAR, COL_INCOME_RANGE] + COLS_MONETARY
 
+
 def get_score(regr, X, y):
     y_pred = regr.predict(X)
-    score = sklearn.metrics.r2_score(y, y_pred)
+    score = r2_score(y, y_pred)
     print("score:", score)
     return score
 
 
-def plot_predictions(regr, X,y):
+def plot_predictions(regr, X, y):
     y_pred = regr.predict(X).flatten()
     print("number of samples:", len(y_pred))
     COL_SOURCE = "source"
@@ -199,7 +199,6 @@ outputs2 = pd.DataFrame({COL_TARGET: y_test, COL_SOURCE: "test (year 2016)"})
 outputs = pd.concat([outputs1, outputs2])
 sns.displot(outputs, x=COL_TARGET, hue=COL_SOURCE, element="step")
 
-
 # %% [markdown]
 # Fit and evaluate a linear model
 
@@ -229,9 +228,219 @@ regr.fit(X_train, y_train)
 print("score on test set:", get_score(regr, X_test, y_test))
 plot_predictions(regr, X_test, y_test)
 
-
 # %% [markdown]
 # Fit and evaluate a multi-layer neural network
+
+
+# %% [markdown]
+# pytorch (lightning) model
+
+# %%
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import collections
+import warnings
+
+import numpy as np
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning import callbacks
+from torch.utils.data import DataLoader, Dataset
+
+NR_OUTP_FEATURES = 1
+
+# DEBUG = True
+DEBUG = False
+NR_DEBUG_SAMPLES = 1000
+BATCH_SIZE = 128
+LEARNING_RATE = 1e-1  # TODO on plateau
+OPT_CRIT_NAME = "val_loss"
+OPT_MODE = "min"
+EARLY_STOP_PATIENCE = 20
+EARLY_STOP_THRESH = 0.0  # reached zero loss
+EARLY_STOP_MIN_DELTA = 1e-3  # accept every improvement
+BATCH_COUNT_EVAL = 10  # evaluating every n-th batch (gradient update) -> save checkpoint
+MAX_EPOCHS = 300  # early stopping should kick in much earlier
+LOSS_FCT = F.mse_loss
+
+if DEBUG:
+    print("INFO: DEBUG mode is on!")
+    EARLY_STOP_PATIENCE = 1
+    MAX_EPOCHS = 2
+
+# Check that MPS is available
+if torch.backends.mps.is_available():
+    device_id = "mps"
+else:
+    if torch.backends.mps.is_built():
+        print(
+            "MPS not available because the current MacOS version is not 12.3+ "
+            "and/or you do not have an MPS-enabled device on this machine."
+        )
+    else:
+        print("MPS not available because the current PyTorch install was not " "built with MPS enabled.")
+    device_id = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f"pytorch: Using {device_id} device")
+torch_device = torch.device(device_id)
+torch.set_default_dtype(torch.float32)
+
+early_stopping_cb = callbacks.EarlyStopping(
+    monitor=OPT_CRIT_NAME,
+    mode=OPT_MODE,
+    min_delta=EARLY_STOP_MIN_DELTA,
+    # number of allowed validation checks with no improvement
+    patience=EARLY_STOP_PATIENCE,
+    # verbose=False,
+    stopping_threshold=EARLY_STOP_THRESH,
+)
+
+training_kws = dict(
+    callbacks=[early_stopping_cb, ],
+    check_val_every_n_epoch=1,
+    # val_check_interval=BATCH_COUNT_EVAL,  # more frequent evaluation
+    max_epochs=MAX_EPOCHS,
+)
+if device_id == "mps":  # Apple Silicon GPU
+    training_kws.update(dict(accelerator='mps', devices=1))
+
+trainer = pl.Trainer(**training_kws)
+
+
+class DataFrameDataset(Dataset):
+    def __init__(self, X: pd.DataFrame, y: pd.DataFrame):
+        super().__init__()
+        self.X = X.values.astype(np.float32)
+        self.y = y.values.astype(np.float32)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        # TODO attention. reset index!
+        sample = self.X[idx]
+        target = self.y[idx]
+        return sample, target
+
+
+class TorchNN(pl.LightningModule):
+    def __init__(
+            self,
+            learning_rate: float,
+            input_size: int,
+            **kwargs,
+    ):
+        super().__init__()
+        # automatically save all the hyperparameters passed to init
+        self.save_hyperparameters()
+
+        self.learning_rate = learning_rate
+        self.input_size = input_size
+        self.output_size = NR_OUTP_FEATURES
+
+        # classification head
+        layers = (
+            # TODO hidden layers,...
+            nn.Linear(input_size, self.output_size),
+            nn.Linear(input_size, self.output_size),
+        )
+        self.nn = nn.Sequential(*layers)
+
+    def get_activations(self, X: torch.Tensor) -> torch.Tensor:
+        return self.nn(X)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        # defines the prediction/inference actions
+        return self.get_activations(X)
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        X = torch.tensor(X.values, dtype=torch.float32)
+        y = self.forward(X).detach().numpy()
+        return y
+
+    def get_loss(self, batch):
+        X, targets = batch
+        activations = self.get_activations(X)
+        return LOSS_FCT(activations, targets)
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop. independent of forward()
+        training_loss = self.get_loss(batch)
+        self.log("train_loss", training_loss)
+        return training_loss
+
+    def validation_step(self, batch, batch_idx):
+        # Note: evaluating a single batch -> the eval sat should only have 1 batch
+        validation_loss = self.get_loss(batch).item()
+        # print(f"\n{validation_loss=}")
+        self.log(OPT_CRIT_NAME, validation_loss)
+
+    def test_step(self, batch, batch_idx):
+        # Note: evaluating a batch, but the eval sat only has 1 batch
+        x_val, y_true = batch
+        y_pred = self(x_val)
+        # TODO evaluate and log metrics
+
+    def configure_optimizers(self):
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        self.reduce_lr_on_plateau = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            self.optimizer,
+            mode='max',
+            factor=0.2,
+            patience=2,
+            min_lr=1e-6,
+            verbose=True
+        )
+        return {
+            "optimizer": self.optimizer,
+            "lr_scheduler": {
+                "scheduler": self.reduce_lr_on_plateau,
+                "monitor": OPT_CRIT_NAME,
+                # If "monitor" references validation metrics, then "frequency" should be set to a
+                # multiple of "trainer.check_val_every_n_epoch".
+                "frequency": 1,
+            }
+        }
+
+
+def get_ds_loader(X, y, batch_size, shuffle):
+    dataset = DataFrameDataset(X, y)
+    ds_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    return ds_loader
+
+
+def train_model():
+    nr_input_features = X_train.shape[1]
+    # data preparation
+    # shuffle each iteration for randomized batches
+    train_loader = get_ds_loader(X_train, y_train, batch_size=BATCH_SIZE, shuffle=True)
+    # only one batch
+    eval_loader = get_ds_loader(X_test, y_test, batch_size=len(y_test), shuffle=False)
+
+    regr = TorchNN(
+        learning_rate=LEARNING_RATE,
+        input_size=nr_input_features,
+    )
+    regr.to(torch_device)
+
+    # fit the head
+    trainer.fit(regr, train_loader, eval_loader)
+    # TODO
+    print("evaluating the evaluation set")
+    trainer.test(regr, eval_loader)
+    return regr
+
+
+regr = train_model()
+print("score on test set:", get_score(regr, X_test, y_test))
+plot_predictions(regr, X_test, y_test)
+
+# %% [markdown]
+# tensorflow
 
 # %%
 from keras import regularizers
@@ -245,7 +454,7 @@ print(f"{len(gpus)} GPUs Available:", gpus)
 from tensorflow import keras
 from tensorflow.keras import layers
 
-LOSS = 'mean_squared_error' # 'mean_absolute_error'
+LOSS = 'mean_squared_error'  # 'mean_absolute_error'
 ACT_FCT = 'relu'
 DROPOUT = 0.0
 LAYER_SIZE = 20
@@ -349,7 +558,6 @@ y_test_pred = regr.predict(X_test).flatten()
 errors = y_test - y_test_pred
 sns.displot(errors, element="step")
 
-
 # %% [markdown]
 # Reduce the dimensionality of the data and visualise it
 
@@ -370,10 +578,10 @@ def reduce_dimensionality(X, nr_dims: int, **h_params):
 
 
 def add_umap_cols(df, columns, **h_params):
-    embeddings = df[columns].values
+    X_orig = df[columns].values
     nr_dims = 2
-    umap_embds = reduce_dimensionality(embeddings, nr_dims=nr_dims, **h_params)
-    for d, umap_emb in enumerate(umap_embds):
+    X_reduced = reduce_dimensionality(X_orig, nr_dims=nr_dims, **h_params)
+    for d, umap_emb in enumerate(X_reduced):
         df[DIM_COLS[d]] = umap_emb
 
 
